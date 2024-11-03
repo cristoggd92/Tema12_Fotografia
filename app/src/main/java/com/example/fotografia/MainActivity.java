78f57be6ebeb1ba23733c5b8a3bc11696dd6f631
Cristopher Gerardo Gaytan Diaz <cristogdiaz@MacBook-Air-de-Cristopher.local>
2024-11-03 17:30:01 -0600

    Initial commit

---
+++ app/src/main/java/com/example/fotografia/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.fotografia;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnCamara;
+    ImageView imgView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnCamara = findViewById(R.id.btnCamara);
+        imgView = findViewById(R.id.imageView);
+
+        btnCamara.setOnClickListener(new View.onClickListener() {
+            @Override
+            public void onClick(View view) { abrirCamara(); }
+        });
+    }
+
+    private void abrirCamara(){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, 1);
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1 && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imagenBitmap = (Bitmap) extras.get("data");
+            imgView.setImageBitmap(imagenBitmap);
+        }
+    }
+}
